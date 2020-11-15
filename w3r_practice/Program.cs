6dec2c332e1a753bbbccb56ecd7bd6ca6a8b0213
Li Lu <meinelily@gmail.com>
2020-11-15 21:26:27 +0100

    add swap and mod

---
+++ w3r_practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace w3r_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
