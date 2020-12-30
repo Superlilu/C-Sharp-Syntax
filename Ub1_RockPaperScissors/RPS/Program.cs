b3ec8c60ba734e9f537b2fb1e44573373393fc41
Li Lu <meinelily@gmail.com>
2020-12-30 19:11:41 +0100

    modified Ub_RPS

--- Ub1_RockPaperScissors/RPS/Program.cs
+++ Ub1_RockPaperScissors/RPS/Program.cs
@@ -6,12 +6,13 @@ namespace RPS
     {
         public int Cscore { get; set; }
         public int Uscore { get; set; }
-        public int Count = 0;
+        public int Count { get; set; }
 
         public Rps()
         {
             Cscore = 0;
             Uscore = 0;
+            Count = 0;
         }
         public enum Role  // enum belongs to class, thus is "static"
         {
@@ -32,11 +33,11 @@ namespace RPS
 
             int returnValue = RandomNumber(MIN, MAX);
 
-            try
-            {
+            //try
+            //{
                 if (returnValue == 1)
                 {
-                    Console.WriteLine("the Computer choses Rock.");
+                    Console.WriteLine("\nComputer choses Rock.");
                     switch (a)
                     {
                         case Role.Rock:
@@ -50,15 +51,15 @@ namespace RPS
                             Console.WriteLine("You chose Scissors, you lose!");
                             Cscore++;
                             break;
-                        default:
-                            throw new Exception("unvalid number!the number must be between 1 - 3!");
+                        //default:
+                        //    throw new Exception("unvalid number!the number must be between 1 - 3!");
                              
 
                     }
                 }
                 else if (returnValue == 2)
                 {
-                    Console.WriteLine("the Computer choses Paper.");
+                    Console.WriteLine("\nComputer choses Paper.");
                     switch ((Role)a)
                     {
                         case Role.Rock:
@@ -72,13 +73,13 @@ namespace RPS
                             Console.WriteLine("You chose Scissors, you win!");
                             Uscore++;
                             break;
-                        default:
-                            throw new Exception("unvalid number!the number must be between 1 - 3!");
+                        //default:
+                        //    throw new Exception("unvalid number!the number must be between 1 - 3!");
                     }
                 }
                 else //(returnValue == 3)
                 {
-                    Console.WriteLine("the Computer choses Scissors.");
+                    Console.WriteLine("\nComputer choses Scissors.");
                     switch (a)
                     {
                         case Role.Rock:
@@ -92,21 +93,21 @@ namespace RPS
                         case Role.Scissors:
                             Console.WriteLine("You chose Scissors, you tied!");
                             break;
-                        default:
-                            throw new Exception("invalid number!the number must be between 1 - 3!");
+                        //default:
+                        //    throw new Exception("invalid number!the number must be between 1 - 3!");
                             // turn into catich after "trow Exception": Count ++ will not be executed!
                     }
                 }
                 Count++;
-            }
+            //}
             //catch (InvalidNumberException ex)
             //{
             //    Console.WriteLine(ex.Message);
             //}
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
         }
     }
 
@@ -126,13 +127,33 @@ namespace RPS
             Console.WriteLine("game begins: 1-Rock, 2-Paper, 3-Scissors");
             while ( game.Uscore < 10 && game.Cscore < 10)
             {
-                Console.Write("\nplease input a number: ");
-                int x = Convert.ToInt32(Console.ReadLine());
-                game.play((Rps.Role)x);   // enum belongs to class, thus is "static" → Rps.Role
-                Console.WriteLine("You have played {0} times. Your score vs Computers score: {1} - {2}", game.Count, game.Uscore, game.Cscore );
-                Console.WriteLine("----------------------------------------------------------------------------");
+                Console.WriteLine("\n----------------------------------------------------\n\nRound: {0}", game.Count + 1);
+                Console.Write("please input a number: ");
+                try
+                {
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    if (!((x == 1) || (x == 2) || (x == 3)))
+                    {
+                        throw new Exception("invalid number!the number must be between 1 - 3!");
+                    }
+                    game.play((Rps.Role)x);   // enum belongs to class, thus is "static" → Rps.Role
+                    Console.WriteLine("\nScore(You vs Computer): {0} - {1}", game.Uscore, game.Cscore);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
             }
-            Console.WriteLine("\nGame Over!\n");
+            Console.WriteLine("\n----------------------------------------------------\nGame Over!");
+            if (game.Cscore > game.Uscore)
+            {
+                Console.WriteLine("Computer achieved 10 points. Computer wins!\n");
+            }
+            else
+            {
+                Console.WriteLine("You achieved 10 points. You win!\n");
+            }
         }
     }
 }
